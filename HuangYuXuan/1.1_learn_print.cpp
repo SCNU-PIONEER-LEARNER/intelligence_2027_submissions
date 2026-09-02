bea3f483b638637f5451044cbfa079bb493e0596
huangHITyx <2660184066@qq.com>
2026-09-03 01:34:50 +0800

    完成1、成为代码高手的第一步

---
+++ HuangYuXuan/1.1_learn_print.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>      
+#include <iostream>   
+using namespace std;   
+
+int main()
+{
+    // 1. 使用 printf 输出 "PIONEER!"
+    printf("PIONEER!\n");
+
+    // 2. 使用 iostream（cout）输出 "PIONEER!"
+    cout << "PIONEER!" << endl;
+
+    return 0;
+}
+
+    
+
+
+
+
+
+
