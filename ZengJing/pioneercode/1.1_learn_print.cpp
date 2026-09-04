143956f60998e1c2156efa78c397d5473bf7fc22
Jingzeng-code <2879369979@qq.com>
2026-09-04 13:43:00 +0800

    add ZengJing assignment (#1) （完成至C++ task）
    完成至C++ task

---
+++ ZengJing/pioneercode/1.1_learn_print.cpp
@@ -0,0 +1,17 @@
+// 1. 使⽤ printf 输出 "PIONEER! "
+                // #include <stdio.h>
+                // int main()
+                // {
+                //     printf("PIONEER!");
+                //     return 0;
+                // }
+// 2. 使⽤ iostream 输出 "PIONEER! "
+                // #include  <iostream>
+                // int main()
+                // {
+                //     std::cout<<"PIONEER!"<<std::endl;
+                //     return 0;
+                // }
+// 3. 简单了解 printf 和 iostream 的区别
+//printf是c语言的输出，利用%d，%f这类的占位符进行输出，在后续引号加上变量，类型不一致的情况下个别情况会自动转换，比如int转成double
+//cout是c++的输出流，不需要占位符进行输出，他能自己进行判断并输出
